09f02d50a2ce57fb04609d3d421c95f2fdc8f651
David Lin <david11yf29@gmail.com>
2024-09-16 12:25:27 -0700

    product service & docker-compose done

---
+++ product-service/src/main/java/com/microservice/service/ProductService.java
@@ -0,0 +1,46 @@
+package com.microservice.service;
+
+import com.microservice.dto.ProductRequest;
+import com.microservice.dto.ProductResponse;
+import com.microservice.model.Product;
+import com.microservice.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public void createProduct(ProductRequest productRequest) {
+        Product product = Product.builder()
+                .name(productRequest.getName())
+                .description(productRequest.getDescription())
+                .price(productRequest.getPrice())
+                .build();
+
+        productRepository.save(product);
+        log.info("Product {} is created and saved", product.getId());
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+
+//        products.stream().map(this::mapToProductResponse);
+        return products.stream().map(product -> mapToProductResponse(product)).toList();
+    }
+
+    private ProductResponse mapToProductResponse(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build();
+    }
+}
