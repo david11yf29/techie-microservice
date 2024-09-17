0cda46afe518b108619430224764962b1c8e8e5f
David Lin <david11yf29@gmail.com>
2024-09-17 01:33:35 -0700

    order service finish coding

---
+++ init-script.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE IF NOT EXISTS `order-service1`;
+USE `order-service1`;
--- product-service/src/test/java/com/microservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/microservice/ProductServiceApplicationTests.java
@@ -3,8 +3,6 @@ package com.microservice;
 import com.microservice.dto.ProductRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -12,7 +10,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.math.BigDecimal;
